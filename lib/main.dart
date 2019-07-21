961aa73fa5239afba007bfd21a753f5c23329106
Skilledtech Imac <skilledtechimac@Skilledtechs-iMac.local>
2019-07-21 10:45:58 +0400

    added article details page

--- lib/main.dart
+++ lib/main.dart
@@ -41,26 +41,53 @@ class _MainPageState extends State<MainPage> {
   }
 }
 
-class LargeScreenHome extends StatelessWidget {
+class LargeScreenHome extends StatefulWidget {
+  @override
+  _LargeScreenHomeState createState() => _LargeScreenHomeState();
+}
+
+class _LargeScreenHomeState extends State<LargeScreenHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: <Widget>[
-          Flexible(child: DrawerNavigation()),
           Flexible(
-            child: ArticlesPage(),
+              child: DrawerNavigation(
+            showBackButton: false,
+          )),
+          verticalDivider(context),
+          Flexible(
+            child: ArticlesPage(
+              onArticleTap: () {
+                setState(() {});
+              },
+            ),
             flex: 2,
           ),
+          verticalDivider(context),
           Flexible(
-            child: Container(
-              color: Colors.black,
-            ),
+            child: ArticleDetailsPage(),
             flex: 3,
           ),
         ],
       ),
     );
+  }
+
+  Container verticalDivider(BuildContext context) {
+    return Container(
+      width: 1,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.grey.withAlpha(50),
+    );
+  }
+}
+
+class ArticleDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("${Random().nextInt(100)}"),),);
   }
 }
 
@@ -73,8 +100,15 @@ class SmallScreenHome extends StatelessWidget {
         elevation: 0,
         iconTheme: IconThemeData(color: Colors.black),
       ),
-      drawer: DrawerNavigation(),
-      body: ArticlesPage(),
+      drawer: DrawerNavigation(
+        showBackButton: true,
+      ),
+      body: ArticlesPage(
+        onArticleTap: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => ArticleDetailsPage()));
+        },
+      ),
     );
   }
 }
@@ -94,7 +128,86 @@ class Article {
   Article(this.title, this.author, this.time);
 }
 
-class ArticlesPage extends StatelessWidget {
+class ArticlesPage extends StatefulWidget {
+  final VoidCallback onArticleTap;
+
+  const ArticlesPage({Key key, this.onArticleTap}) : super(key: key);
+
+  @override
+  _ArticlesPageState createState() => _ArticlesPageState();
+}
+
+class _ArticlesPageState extends State<ArticlesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(kToolbarHeight),
+            child: Container(
+              height: 50.0,
+              child: TabBar(
+                tabs: [
+                  Tab(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: Text(
+                        'Latest',
+                        style: Theme.of(context)
+                            .textTheme
+                            .body1
+                            .copyWith(color: Colors.green),
+                      ),
+                    ),
+                  ),
+                  Tab(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: Text(
+                        'Popular',
+                        style: Theme.of(context)
+                            .textTheme
+                            .body1
+                            .copyWith(color: Colors.green),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              ArticleListWidget(
+                onArticleTap: () {
+                  widget.onArticleTap();
+                },
+              ),
+              ArticleListWidget(
+                onArticleTap: () {
+                  widget.onArticleTap();
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ArticleListWidget extends StatefulWidget {
+  final VoidCallback onArticleTap;
+
+  const ArticleListWidget({Key key, this.onArticleTap}) : super(key: key);
+
+  @override
+  _ArticleListWidgetState createState() => _ArticleListWidgetState();
+}
+
+class _ArticleListWidgetState extends State<ArticleListWidget> {
   final List<Article> articles = [
     Article("New Startup idea", "Mega", "${Random().nextInt(24)} HOURS AGO"),
     Article("New Startup idea", "Mega", "${Random().nextInt(24)} HOURS AGO"),
@@ -108,38 +221,70 @@ class ArticlesPage extends StatelessWidget {
     Article("New Startup idea", "Mega", "${Random().nextInt(24)} HOURS AGO"),
     Article("New Startup idea", "Mega", "${Random().nextInt(24)} HOURS AGO"),
   ];
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(kToolbarHeight),
-            child: Container(
-              height: 50.0,
-              child: TabBar(
-                tabs: [
-                  Tab(text: 'Latest',),
-                  Tab(text: 'Popular'),
-                ],
-              ),
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return InkWell(
+          onTap: () {
+            widget.onArticleTap();
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Colors.red)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        articles[index].time,
+                        style: Theme.of(context).textTheme.caption,
+                        maxLines: 1,
+                      ),
+                      Text(
+                        articles[index].title,
+                        style: Theme.of(context).textTheme.title,
+                        maxLines: 1,
+                      ),
+                      Text.rich(
+                        TextSpan(children: [
+                          TextSpan(
+                              text: 'by ',
+                              style: Theme.of(context).textTheme.caption),
+                          TextSpan(
+                              text: articles[index].author,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .body1
+                                  .copyWith(color: Colors.green))
+                        ]),
+                        maxLines: 1,
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-          body: TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-            ],
-          ),
-        ),
-      ),
+        );
+      },
+      itemCount: articles.length,
     );
   }
 }
 
 class DrawerNavigation extends StatelessWidget {
+  final bool showBackButton;
   final List<NavigationItem> navigationItems = [
     NavigationItem("HOME", Icons.home),
     NavigationItem("NEWS", Icons.home),
@@ -152,6 +297,8 @@ class DrawerNavigation extends StatelessWidget {
     NavigationItem("EVENTS", Icons.home),
   ];
 
+  DrawerNavigation({Key key, this.showBackButton}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -159,16 +306,21 @@ class DrawerNavigation extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: BackButton(),
+          Visibility(
+            visible: showBackButton,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: BackButton(),
+            ),
           ),
           Expanded(
             child: ListView.builder(
               itemCount: navigationItems.length,
               itemBuilder: (BuildContext context, int index) {
                 return DrawerNavigationItemWidget(
-                    navigationItem: navigationItems[index]);
+                  navigationItem: navigationItems[index],
+                  active: index == 0,
+                );
               },
             ),
           ),
@@ -179,9 +331,12 @@ class DrawerNavigation extends StatelessWidget {
 }
 
 class DrawerNavigationItemWidget extends StatelessWidget {
+  final bool active;
+
   const DrawerNavigationItemWidget({
     Key key,
     @required this.navigationItem,
+    this.active,
   }) : super(key: key);
 
   final NavigationItem navigationItem;
@@ -189,6 +344,7 @@ class DrawerNavigationItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      selected: active,
       trailing: Icon(navigationItem.icon),
       title: Text(
         navigationItem.title,
@@ -196,4 +352,8 @@ class DrawerNavigationItemWidget extends StatelessWidget {
       ),
     );
   }
+}
+
+bool isBigScreen(BuildContext context) {
+  return MediaQuery.of(context).size.width > 600;
 }
